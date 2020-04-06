91b794066ccabcdc714a5244a93399857a2741aa
Daniel Reis <dreis@opensourceintegrators.com>
2020-04-06 11:45:36 +0100

    [FIX] Log message for Company missing configuration

--- avatax_connector/models/res_company.py
+++ avatax_connector/models/res_company.py
@@ -20,7 +20,7 @@ class Company(models.Model):
             _LOGGER.warn(
                 _("Company %s has too many Avatax configurations!"),
                 self.display_name)
-        if len(res) == 1:
+        if len(res) == 0:
             _LOGGER.warn(
                 _("Company %s has no Avatax configuration."),
                 self.display_name)
